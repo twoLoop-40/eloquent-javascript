a986c758371a62a12c695e7e99d1bf1421520777
Joon Ho Lee <jh.lee@spira-t.com>
2022-03-27 16:28:44 +0900

    usingFetch

---
+++ chapter18/usingFetch.js
@@ -0,0 +1,42 @@
+function requestFetch(url) {
+  class Request {
+    static create(url) {
+      return new Request(url);
+    }
+    fetch() {
+      const { url, headers } = { ...this };
+      return fetch(url, { headers });
+    }
+    setContentType(type = "") {
+      this.headers = { accept: type };
+      return this;
+    }
+    setUrl(url) {
+      this.url = url;
+      return this;
+    }
+    constructor(url) {
+      this.headers = {};
+      this.url = url;
+    }
+  }
+  const types = ["text/plain", "text/html", "application/json"];
+  return Promise.all(
+    types
+      .map((type) => {
+        return Request.create(url).setContentType(type);
+      })
+      .map((req) => req.fetch())
+  ).then((resps) =>
+    Promise.all(resps.map((resp) => resp.text())).then((texts) =>
+      texts.map((text) => console.log(text))
+    )
+  );
+}
+
+function usingFetch() {
+  const URL = "https://eloquentjavascript.net/author";
+  requestFetch(URL);
+}
+
+usingFetch();
